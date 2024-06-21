082288d97c1b0750a6c403652175b9d9f37b5e23
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-21 16:21:18 +0300

    1.3 arithmetic operations v3

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,8 +1,7 @@
 package ru.calculator;
 
 public class Calculator {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int one = 1;
         int two = 2;
         int six = 6;
