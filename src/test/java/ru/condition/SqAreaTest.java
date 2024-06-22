3025b3003524be415ad6eadb8f1af7f3dc911e0f
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-22 21:48:41 +0300

    2.6 the test for the side of the triangle

---
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -0,0 +1,34 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP10K3Square4Dote69() {
+        double expected = 4.69;
+        int p = 10;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP2K1Square0Dote25() {
+        double expected = 0.25;
+        int p = 2;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
