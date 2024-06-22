0b95f6f3b89e1b87f73558c8c7f358a37c8c6039
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-22 21:40:30 +0300

    2.5 a test to determine the distance

---
+++ src/test/java/ru/condition/PointTest.java
@@ -0,0 +1,52 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when51to24then2() {
+        double expected = 4.24;
+        int x1 = 5;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when30to10then2() {
+        double expected = 2;
+        int x1 = 3;
+        int y1 = 0;
+        int x2 = 1;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when11to77then2() {
+        double expected = 8.48;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 7;
+        int y2 = 7;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+}
