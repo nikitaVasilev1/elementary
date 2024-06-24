34338bddac9fb5ae43fa557b23d35a32655cc100
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-25 00:54:59 +0300

    4.5 factorial

---
+++ src/test/java/ru/loop/FactorialTest.java
@@ -0,0 +1,23 @@
+package ru.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int expected = 120;
+        int n = 5;
+        int out = Factorial.calc(n);
+        assertThat(out).isEqualTo(expected);
+    }
+    @Test
+    void whenCalculateFactorialForZeroThenOne() {
+        int expected = 1;
+        int n = 1;
+        int out = Factorial.calc(n);
+        assertThat(out).isEqualTo(expected);
+    }
+}
