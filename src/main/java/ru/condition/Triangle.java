d607aa91b092ece58c3a860e6b4847263945c3e4
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-24 23:52:00 +0300

    3.13 multiple logical and v2

--- src/main/java/ru/condition/Triangle.java
+++ src/main/java/ru/condition/Triangle.java
@@ -3,6 +3,6 @@ package ru.condition;
 public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
-        return (ab + ac > bc) && (ab + bc > ac) && (bc + ac > ac);
+        return ab + ac > bc && ab + bc > ac && bc + ac > ac;
     }
 }
