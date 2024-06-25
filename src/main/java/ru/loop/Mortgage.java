7c87617c53dfc5cb2707e026a79584b55004e563
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-25 21:53:19 +0300

    4.12 pseudographics v2

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt = 0;
-        debt = amount + amount * (percent / 100);
-        while (debt > 0) {
-            debt -= salary;
+        while (amount > 0) {
+            amount += amount * (percent / 100);
+            amount -= salary;
             year++;
         }
         return year;
