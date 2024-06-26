e8bdb4fe4bc661d5f0d9f683388c7c7506790fc7
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-26 18:37:33 +0300

    5.3 filling the array

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -5,6 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ваня ";
+        names[1] = "Маша";
+        names[2] = "Ваня";
+        names[3] = "Коля";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
         System.out.println("Размер массива ages = " + ages.length);
         System.out.println("Размер массива surname = " + surname.length);
         System.out.println("Размер массива prices = " + prices.length);
