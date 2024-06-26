0a54c514ddd1bdf1d0e4d865177d0e82a2812e4d
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-26 18:22:46 +0300

    4.8 break v5

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -2,13 +2,11 @@ package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            prime = false;
-        }
+        boolean prime = number != 1;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
+                break;
             }
         }
         return prime;
