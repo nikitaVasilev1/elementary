3df52735cce0038b2494b6808d1235a3010f44a4
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-26 18:42:34 +0300

    5.4 array and for v2

--- src/main/java/ru/array/ArrayLoop.java
+++ src/main/java/ru/array/ArrayLoop.java
@@ -1,2 +1,13 @@
-package ru.array;public class ArrayLoop {
+package ru.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 2 + 3;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
 }
