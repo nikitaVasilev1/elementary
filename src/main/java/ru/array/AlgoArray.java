c44464530812e91e7396e87c30f1a8c469699a66
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-26 21:10:50 +0300

    5.9 algoArray

---
+++ src/main/java/ru/array/AlgoArray.java
@@ -0,0 +1,20 @@
+package ru.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 3, 2, 1, 4};
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
