d74151ffcccb15001b19dbb0ccbdc2e06734e127
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-27 19:04:02 +0300

    5.14 array char

--- src/main/java/ru/array/Check.java
+++ src/main/java/ru/array/Check.java
@@ -1,2 +1,14 @@
-package ru.array;public class Check {
+package ru.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length - 2; i++) {
+            if (data[i] != data[i + 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
