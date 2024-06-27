9776bb19493d677b63f840219c8cf5c201b19792
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-27 18:40:03 +0300

    5.11 swap

--- src/main/java/ru/array/SwitchArray.java
+++ src/main/java/ru/array/SwitchArray.java
@@ -9,8 +9,15 @@ public class SwitchArray {
         return array;
     }
 
+    public static int[] swap(int[] array, int source, int dest) {
+        int number = array[dest];
+        array[dest] = array[source];
+        array[source] = number;
+        return array;
+    }
+
     public static void main(String[] args) {
-        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
