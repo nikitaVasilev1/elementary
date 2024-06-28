17bc92fb8c0355541aacafaeee894cdff4533900
Nikita Vasilev <nikita.vas@yandex.ru>
2024-06-28 17:05:14 +0300

    5.22 sequence

---
+++ src/main/java/ru/array/Sequence.java
@@ -0,0 +1,16 @@
+package ru.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 5;
+        array[1][1] = 4;
+        array[1][2] = 8;
+        array[2][0] = 6;
+        array[2][1] = 21;
+        array[2][2] = 51;
+    }
+}
